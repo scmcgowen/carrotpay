94c15a18ac5ea778cec34fb3c2c5503709b5dd14
BuildTools <unconfigured@null.spigotmc.org>
2023-07-20 16:16:30 -0700

    Feature: CarrotPay will now look for transactions made to the wallet Rewrote CarrotPay to use the krist websocket API rather than the HTTP api.

--- carrotpay.lua
+++ carrotpay.lua
@@ -3,6 +3,7 @@ if not switchcraft then
     error("Must run on SwitchCraft 3")
 end
 sleep(1)
+local sha256 = require("sha256")
 if not chatbox or not chatbox.hasCapability("command") or not chatbox.hasCapability("tell") then
 term.setBackgroundColor(colors.black)
 term.clear()
@@ -67,11 +68,22 @@ os.pullEvent("key")
 os.reboot()
 end
 local owner =chatbox.getLicenseOwner()
-local sha256 = require("sha256")
 local function makeaddressbyte(byte)
     local byte = 48 + math.floor(byte/7)
     return string.char(byte + 39 > 122 and 101 or byte > 57 and byte + 39 or byte)
 end
+local expect = require("cc.expect").expect
+local function split(inputstr, sep)
+    expect(1,inputstr,"string")
+    expect(1,sep,"string","nil")
+    sep = sep or ","
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+      table.insert(t, str)
+    end
+    return t
+  end
+  
 local function make_address(key)
   local protein = {}
   local stick = sha256(sha256(key))
@@ -98,9 +110,8 @@ local function make_address(key)
 end
 local address = make_address(pkey)
 local function get_balance(addr)
-    local a = http.get("https://krist.dev/addresses/"..addr)
-    local balance = textutils.unserializeJSON(a.readAll()).address.balance
-    a.close()
+    os.queueEvent("get_balance",addr)
+    _,balance = os.pullEvent("balance")
     return balance
 end
 local function pay(to,amt,mta)
@@ -111,15 +122,93 @@ local function pay(to,amt,mta)
         chatbox.tell(owner,"Transaction failed. Not enough Krist","&6CarrotPay")
         return false
     else
-        request = textutils.serialiseJSON{
-            privatekey = pkey,
-            to = to,
-            amount=amt,
-            metadata=mta
-        }
-        r,err = http.post("https://krist.dev/transactions",request,{["Content-Type"]="application/json"})
-        --print(r,err)
-        if r then return true else return false end
+        os.queueEvent("make_transaction",to,amt,mta)
+        _,ok = os.pullEvent("transaction_complete")
+        return ok
+    end
+end
+local function handleWebSockets()
+    local id = -1
+    local r = http.post("https://krist.dev/ws/start","{\"privatekey\":\""..pkey.."\"}",{["content-type"]="application/json"})
+    local resp = textutils.unserialiseJSON(r.readAll())
+    r.close()
+    r = nil
+    if  resp.ok then
+        local socket = http.websocket(resp.url)
+        id = id + 1
+        socket.send('{\"id\":'..id ..',\"type\":\"subscribe\",\"event\":\"ownTransactions\"}')
+        while true do
+            event = {os.pullEvent()}
+            if event[1] == "websocket_message" then
+                if event[2] == resp.url then
+                    wsevent = textutils.unserialiseJSON(event[3])
+                    if wsevent.event == "transaction" and wsevent.transaction.to == address then
+                        local from = wsevent.transaction.from
+                        local hasMessage = false
+                        local hasError = false
+                        local message = ""
+                        err = ""
+                        if wsevent.transaction.metadata then
+                            mta = split(wsevent.transaction.metadata,";")
+                            for i,p in pairs(mta) do
+                                if p:match("message") then
+                                    message = split(p,"=")[2]
+                                    hasMessage = true
+                                end
+                                if p:match("error") then
+                                    err = split(p,"=")[2]
+                                    hasError = true
+                                end
+                                if p:match("username") then
+                                    from = split(p,"=")[2].." ("..wsevent.transaction.from..")"
+                                end
+                            end
+                        end
+                        msg2 = "You have received K"..wsevent.transaction.value.." from "..from
+                        if hasMessage or hasError then
+                            if hasMessage then
+                                msg2 = msg2.."\nMessage: "..message
+                            end
+                            if hasError then
+                                msg2 = msg2.."\nError: "..err
+                            end
+                        end
+                        chatbox.tell(owner,msg2,"&6CarrotPay")
+                    elseif wsevent.type == "keepalive" or wsevent.type == "response" then
+                    else
+                    end
+                end
+            elseif event[1] == "get_balance" then
+                id = id + 1
+                local rq = {
+                    id = id,
+                    type="address",
+                    address=event[2]
+                } 
+                socket.send(textutils.serialiseJSON(rq))
+                rspt =socket.receive()
+                rsp = textutils.unserialiseJSON(rspt)
+                os.queueEvent("balance",rsp.address.balance)
+
+            elseif event[1] == "make_transaction" then
+                id = id + 1
+                local rq = {
+                    id = id,
+                    type="make_transaction",
+                    to = event[2],
+                    amount = event[3],
+                    metadata = event[4],
+                }
+                socket.send(textutils.serialiseJSON(rq))
+                local c
+                repeat
+                c = socket.receive()
+                c = textutils.unserialiseJSON(c)
+                until c.type == "response"
+                os.queueEvent("transaction_complete",c.ok)
+                
+            end
+        ends
     end
 end
 local function handleCommands()
@@ -127,8 +216,8 @@ local function handleCommands()
         local command = {os.pullEvent("command")}
         if command[3] == "pay" and command[5].ownerOnly then
             if command[4][1] == "--update" then
-            shell.run("wget https://raw.githubusercontent.com/scmcgowen/carrotpay/main/carrotpay.lua "..shell.getRunningProgram)
-            os.reboot()
+                shell.run("wget https://raw.githubusercontent.com/scmcgowen/carrotpay/main/carrotpay.lua "..shell.getRunningProgram)
+                os.reboot()
             end
             if not command[4][1] then
                 chatbox.tell(command[2],"Specify who to pay","&6CarrotPay")
@@ -139,8 +228,6 @@ local function handleCommands()
                 command_copy = {table.unpack(command)}
                 mta = table.concat({select(3, unpack(command_copy[4]))}, " ")
                 local s = command[4][1]
-                -- require("cc.pretty").pretty_print(s)
-                --print(#s)
                 if ((s:match("^k[a-z0-9]+$") or s:match("^[a-f0-9]+$")) and #s == 10) or (s:match("^[a-z0-9]@[a-z0-9]+$")) or s:match("^[%a%d]+@[%a%d]+%.kst$") or s:match("^[%a%d]+%.kst") then
                     c = pay(s,command[4][2],mta)
                     if c then
@@ -161,7 +248,15 @@ local function handleCommands()
                     chatbox.tell(command[2],"There was an error.","&6CarrotPay")
                 end
             end
+        elseif command[3] == "request" and command[5].ownerOnly then
+            msg = table.concat({select(3,unpack(command[4]))}, " ")
+            if msg == "" then
+            chatbox.tell(command[4][1],owner.." wants K"..command[4][2].." from you.\nPay to "..address,"&6CarrotPay")
+            else
+            chatbox.tell(command[4][1],owner.." wants K"..command[4][2].." from you.\nPay to "..address.."\nMessage: "..msg,"&6CarrotPay")
+            end
+            chatbox.tell(owner,"Requested K"..command[4][2].." from "..command[4][1],"&6CarrotPay")
         end
     end
 end
-handleCommands()
+parallel.waitForAny(handleWebSockets,handleCommands)
